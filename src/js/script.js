942c017028b5d537e5509090724041bbedec1470
Lukasz Kallas <lukasz.kallas@gmail.com>
2022-09-07 10:27:45 +0200

    Add favourites

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,7 @@
 	const booksData = dataSource.books;
 	const booksContainer = document.querySelector(select.containerOf.books);
 	const template = Handlebars.compile(document.querySelector(select.templateOf.menuBooks).innerHTML);
+	const favoriteBooks = [];
 
 	const renderBooks = () => {
 		booksData.forEach((book) => {
@@ -21,5 +22,20 @@
 			booksContainer.appendChild(element);
 		});
 	};
+
+	const initActions = () => {};
+	booksContainer.addEventListener('dblclick', function (event) {
+		event.preventDefault();
+		const bookLinkHTML = event.target.offsetParent;
+		const bookId = bookLinkHTML.getAttribute('data-id');
+		if (favoriteBooks.indexOf(bookId) == -1) {
+			favoriteBooks.push(bookId);
+			bookLinkHTML.classList.add('favorite');
+		} else {
+			favoriteBooks.splice(favoriteBooks.indexOf(bookId), 1);
+			bookLinkHTML.classList.remove('favorite');
+		}
+	});
 	renderBooks();
+	initActions();
 }
